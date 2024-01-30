93e59ffd06012f43ff67e8854bd8a8fa8822538b
Steven Lam <stevenl.inbox@gmail.com>
2024-01-30 09:32:53 -0800

    Finished navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 import '../CSS/NavBar.css';
+import logo from '../Images/ClubLogo.png'
 
 export default function NavBar() {
   return (
     <nav className="navBar">
+      <img src={logo} alt="Club logo" width='25%'/>
       <h1>TPC Chico</h1>
       <h2>Technical Projects Club</h2>
       <ul>
@@ -14,6 +16,7 @@ export default function NavBar() {
         <CustomLink to="/Contact">Contact Us</CustomLink>
         <CustomLink to="/Events">Events</CustomLink>
       </ul>
+      <a href="https://discord.gg/gZb7q8S7JY" target='_blank' rel="noopener noreferrer" class="cta-button">Join Our Discord</a>
     </nav>
   );
 }
