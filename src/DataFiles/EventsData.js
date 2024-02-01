28a8b8922fd5c2a70d47954be3a1d9df84928c98
Steven Lam <stevenl.inbox@gmail.com>
2024-02-01 00:46:51 -0800

    Finished events page

---
+++ src/DataFiles/EventsData.js
@@ -0,0 +1,29 @@
+const eventsData = [
+    {
+      date: '2024-02-14',
+      title: 'General Club Meetup',
+      description: 'First general meeting for the semester. And guess what? There will be PIZZA! We are a growing community where members can showcase their projects, connect, and collaborate. We hope to see you there!',
+      images: []
+    },
+    {
+      date: '2024-03-14',
+      title: 'General Club Meeting',
+      description: 'Second general meeting for the semester. And guess what? There will be PIZZA! We are a growing community where members can showcase their projects, connect, and collaborate. We hope to see you there!',
+      images: []
+    },
+    {
+      date: '2024-04-11',
+      title: 'General Meetup',
+      description: 'Third general meeting for the semester. And guess what? There will be PIZZA! We are a growing community where members can showcase their projects, connect, and collaborate. We hope to see you there!',
+      images: []
+    },
+    {
+        date: '2024-05-09',
+        title: 'General Meetup',
+        description: 'Fourth general meeting for the semester. And guess what? There will be PIZZA! We are a growing community where members can showcase their projects, connect, and collaborate. We hope to see you there!',
+        images: []
+      },
+  ];
+  
+  export default eventsData;
+  
--- src/Pages/Events.js
+++ src/Pages/Events.js
@@ -1,7 +1,51 @@
+import React, { useState } from 'react';
+import '../CSS/Events.css';
+import eventsData from '../DataFiles/EventsData';
+import Calendar from 'react-calendar';
+
 function Events() {
+    const [value, setValue] = useState(new Date());
+
+    function onChange(nextValue) {
+        setValue(nextValue);
+    }
+
+    function tileContent({ date, view }) {
+        const formattedDate = date.toISOString().split('T')[0]; // Convert date to ISO format
+        const eventsForDay = eventsData.filter((event) => event.date === formattedDate);
+        if (eventsForDay.length > 0) {
+            return (
+                <div>
+                    {eventsForDay.map((event, index) => (
+                        <p key={index}>{event.title}</p>
+                    ))}
+                </div>
+            );
+        }
+        return null;
+    }
+
+
     return (
-        <h1>Events</h1>
-    )
+        <div className="EventBody">
+            <br></br>
+            <Calendar
+                onChange={onChange}
+                value={value}
+                className="Calendar"
+                tileContent={tileContent}
+            />
+            <ul className="EventList">
+                {eventsData.map((event, index) => (
+                    <li key={index} className="EventElement">
+                        <strong>Date:</strong> {event.date}<br />
+                        <strong>Title:</strong> {event.title}<br />
+                        <strong>Description:</strong> {event.description}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default Events;
